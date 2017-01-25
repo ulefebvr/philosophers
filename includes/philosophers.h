d7a0da7295db43b649c857b3581132dffa0551fc
ulefebvr <ulefebvr@student.42.fr>
2017-01-25 17:00:00 +0100

    [HEADER] Set some MACRO

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -1,4 +1,30 @@
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
+/*
+** Le nombre de points de vie maximum des philosophes.
+*/
+# define MAX_LIFE				30
+
+/*
+** Le nombre de SECONDES que met un philosophe à manger.
+*/
+# define EAT_T					3
+
+/*
+** Le nombre de SECONDES pendant lesquels un philosophe se repose.
+*/
+# define REST_T					3
+
+/*
+** Le nombre de SECONDES pendant lesquels un philosophe réfléchit.
+*/
+# define THINK_T				3
+
+/*
+** Le temps en SECONDES après lesquels la simulation s’interrompt
+** en affichant “Now, it is time... To DAAAAAAAANCE ! ! !”
+*/
+# define TIMEOUT				60
+
 #endif
